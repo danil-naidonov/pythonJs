5203618cff0ab1aae2bc2d86758ee1366c4dbd69
danil-naidonov <danylo.naidonov@guesty.com>
2023-12-22 22:04:04 +0200

    Init project

---
+++ script.js
@@ -0,0 +1,27 @@
+// Number 1
+// BigInt 1e100
+// String "some text"
+// Boolean false/true
+// Null
+// Undefined
+// Object (Array, Object, Function)
+// Symbol
+
+
+// maxLEngthConstanta = 10;
+
+// let a = Number(prompt('Enter any number'))
+// let b = 2;
+
+// if(isNaN(a)) {
+//     alert('Entrered value is NaN')
+//     throw new Error('asdasd')
+// }
+
+// if((a === b)) {
+//     alert('You won')
+// } else {
+//     alert('You lost')
+// }
+
+
